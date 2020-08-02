27223a79aec1ba23eba25347ef50537735ba08b6
Kirill Artimenia <kirill.artimenya@mail.ru>
2020-08-02 18:26:34 +0300

    add MASTER CHEF, BOOK A TABLE, DELICIOUS MENU

--- js/main.js
+++ js/main.js
@@ -7,3 +7,41 @@ menuIcon.addEventListener('click', () =>{
   menuBbody.classList.toggle("active");
   headerLogo.classList.toggle("active");
 });
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function minusSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("item"),
+        dots = document.getElementsByClassName("slider-dots_item"),
+        i
+
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
